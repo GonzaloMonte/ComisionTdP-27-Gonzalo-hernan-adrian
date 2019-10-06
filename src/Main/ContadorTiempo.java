c0e02dc2d0d1062ba8feb0c5c9af807461cfe779
Nano3114 <54462455+Nano3114@users.noreply.github.com>
2019-10-06 14:53:46 -0300

    Update ContadorTiempo.java
    este contador tiempo podria controlar ,cuando comienze el juego luego del pantallazo de inicio(luego de apretar play)

--- src/Main/ContadorTiempo.java
+++ src/Main/ContadorTiempo.java
@@ -1,30 +1,13 @@
 package Main;
 
-import Personajes.*;
-public class ContadorTiempo extends Thread {
-	protected Ventana ventana;
-	protected Mapa mapa;
-	
-	ContadorTiempo(Ventana v,Mapa m){
-		ventana=v;
-		mapa=m;
-	}
+public class ContadorTiempo extends Thread{
 	public void run(){
-		ventana.empezarOleada();
-		while(true){
+		while(true)
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			if (!(mapa.listaEnemigosVivos().isEmpty()))
-			for (Enemigo e: mapa.listaEnemigosVivos()) {
-			
-			ventana.limpiarCelda(e.getX(),e.getY());
-			e.mover();
-			ventana.colocarEnemigo(e);
-			
-			}
-		}	
-	}	
+	}
 }
